8dff18c58c89dad4c9917011f7832a8f83d9d50b
ehailu675 <elisabethahailu@gmail.com>
2018-07-25 19:50:21 +0000

    Done.

---
+++ practice.rb
@@ -0,0 +1,12 @@
+# num_of_hotdogs_eaten = 3
+#   while num_of_hotdogs_eaten < 10
+#     num_of_hotdogs_eaten += 2
+#     puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
+#   end
+#   puts "You ate a total of #{num_of_hotdogs_eaten} hot dogs!"
+
+counter = 0
+  until counter == 20
+    puts "The current number is less than 20."
+    counter += 1
+  end
