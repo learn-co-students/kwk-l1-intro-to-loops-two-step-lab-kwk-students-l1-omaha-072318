e75d232b0db86ff124c40c3955a6479743000e35
art123craft <cammiebluecammie@gmail.com>
2018-07-26 13:58:24 +0000

    Done.

---
+++ loop.rb
@@ -0,0 +1,30 @@
+# loop do
+#   puts " Now watch me whip."
+#   puts "Watch me nae nae."
+#   puts "Now watch me whip, whip."
+#   puts "Now watch me nae nae."
+#   puts "Oooooh, watch me, watch me."
+#   puts "Oooooh, watch me, watch me."
+#   puts "Oooooh, watch me, watch me."
+# break
+# end
+# puts "Do the skanky leg."
+# puts "Do the skanky leg."
+# exit
+
+# counter = 0
+# loop do
+#   counter += 1
+# puts "ineration #{counter} of the loop"
+# if counter >= 10
+#   break
+# end
+# end
+
+
+
+
+
+
+
+
